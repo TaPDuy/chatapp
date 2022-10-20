bbbb3515bc6bd63e3181daf6e26d8e92268514fd
TaPDuy <tpduy2001@gmail.com>
2022-10-20 14:37:08 +0700

    Create maven projects

---
+++ ChaTapp-Client/src/main/java/nhom12/chatapp/client/EntryPoint.java
@@ -0,0 +1,7 @@
+package nhom12.chatapp.client;
+
+public class EntryPoint {
+    public static void main(String[] args) {
+	
+    }
+}
---
+++ ChaTapp-Server/src/main/java/nhom12/chatapp/server/EntryPoint.java
@@ -0,0 +1,7 @@
+package nhom12.chatapp.server;
+
+public class EntryPoint {
+    public static void main(String[] args) {
+	
+    }
+}

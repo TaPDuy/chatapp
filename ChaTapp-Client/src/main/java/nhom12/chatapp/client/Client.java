5dd445bbc2e72d7e0c0b2a04ed62b065c185d6da
TaPDuy <tpduy2001@gmail.com>
2022-10-27 17:48:21 +0700

    Rename EntryPoint

--- ChaTapp-Client/src/main/java/nhom12/chatapp/client/Client.java
+++ ChaTapp-Client/src/main/java/nhom12/chatapp/client/Client.java
@@ -3,19 +3,16 @@ package nhom12.chatapp.client;
 import java.io.IOException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import nhom12.chatapp.client.controller.LoginController;
 import nhom12.chatapp.client.controller.WindowController;
-import nhom12.chatapp.client.view.LoginFrm;
-import nhom12.chatapp.client.controller.ChatClient;
 
-public class EntryPoint {
+public class Client {
     
     public static void main(String[] args) {
 	ServerConnection server = null;
 	try {
 	    server = new ServerConnection("localhost", 7777);
 	} catch (IOException ex) {
-	    Logger.getLogger(EntryPoint.class.getName()).log(Level.SEVERE, null, ex);
+	    Logger.getLogger(Client.class.getName()).log(Level.SEVERE, null, ex);
 	}
 	
 	if(server != null) {

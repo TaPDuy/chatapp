e98567d0e9e63f63aa3c5d21dce1ae0eb2dfa2f5
TaPDuy <tpduy2001@gmail.com>
2022-11-02 01:41:35 +0700

    Rewrite DAOs

--- ChaTapp-Server/src/main/java/nhom12/chatapp/server/dao/GroupDAO.java
+++ ChaTapp-Server/src/main/java/nhom12/chatapp/server/dao/GroupDAO.java
@@ -1,76 +1,181 @@
 package nhom12.chatapp.server.dao;
 
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
 import nhom12.chatapp.model.Group;
-import static nhom12.chatapp.server.dao.DAO.con;
+import nhom12.chatapp.util.ConsoleLogger;
+import nhom12.hibernate.util.JPAUtil;
 
-public class GroupDAO extends DAO {
 
-    private static final String CHECK_EXIST = "SELECT * FROM tblgroup WHERE name = ? limit 1";
-    private static final String INSERT_GROUP = "INSERT INTO tblgroup(name) VALUES (?)";
-    private static final String GET_BY_ID = "SELECT * FROM tblgroup WHERE id = ?";
-    
+public class GroupDAO implements DAO<Group> {
+
     public GroupDAO() {
-	super();
+	
     }
-    
+
     public boolean checkExist(Group group) {
 	
-        try {
-            PreparedStatement ps = con.prepareStatement(CHECK_EXIST);
-            ps.setString(1, group.getName());
-            
-	    System.out.println("[DB]: Executing sql statement '" + ps.toString() + "'");
-	    ResultSet rs = ps.executeQuery();
-            return rs.next();
-	    
-        } catch (SQLException ex) {
-	    System.out.println("[DB]: Could not perform checkExist for " + group.toString());
-            Logger.getLogger(UserDAO.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        return false;
-    }
-    
-    public Group getGroupByID (int id) {
+	EntityManager entityManager = JPAUtil.getEntityManagerFactory().createEntityManager();
 	
 	try {
 	    
-	    PreparedStatement ps = con.prepareStatement(CHECK_EXIST);
-	    ps.setInt(1, id);
+	    entityManager.getTransaction().begin();
 	    
-	    Group group = new Group();
-	    ResultSet rs = ps.executeQuery();
-	    if(rs.next()) {
-		group.setId(rs.getInt("id"));
-		group.setName(rs.getString("name"));
+	    CriteriaBuilder cb = entityManager.getCriteriaBuilder();
+	    CriteriaQuery<Group> cr = cb.createQuery(Group.class);
+	    Root<Group> root = cr.from(Group.class);
+
+	    cr.select(root).where(cb.and(cb.equal(root.get("id"), group.getId())
+		)
+	    );
+
+	    Query query = entityManager.createQuery(cr);
+	    boolean result = query.getResultList().size() == 1;
+	    
+	    entityManager.getTransaction().commit();
+	    entityManager.close();
+	    return result;
+
+	} catch (Exception ex) {
+	    
+	    ConsoleLogger.log("Failed trying to check if user exist: " + group.toString(), 
+		"DAO", 
+		ConsoleLogger.ERROR
+	    );
+	    entityManager.close();
+	}
+	
+	return false;
+    }
+    
+    @Override
+    public Group findById(int id) {
+	
+	Group result;
+	EntityManager entityManager = JPAUtil.getEntityManagerFactory().createEntityManager();
+	
+	try {
+	    
+	    entityManager.getTransaction().begin();
+	    
+	    result = entityManager.getReference(Group.class, id);
+	    
+	    entityManager.getTransaction().commit();
+	    entityManager.close();
+
+	} catch (Exception ex) {
+	    ConsoleLogger.log("Failed trying to get group of id: " + id, "DAO", ConsoleLogger.ERROR);
+	    entityManager.close();
+	    return null;
+	}
+	
+	return result;
+	
+    }
+    
+    @Override
+    public List<Group> findAll() {
+	
+	List<Group> results;
+	EntityManager entityManager = JPAUtil.getEntityManagerFactory().createEntityManager();
+	
+	try {
+	    
+	    entityManager.getTransaction().begin(); {
+
+		CriteriaBuilder cb = entityManager.getCriteriaBuilder();
+		CriteriaQuery<Group> cr = cb.createQuery(Group.class);
+		cr.select(cr.from(Group.class));
+
+		Query query = entityManager.createQuery(cr);
+		results = query.getResultList();
 	    }
 	    
-	} catch (SQLException ex) {
-	    Logger.getLogger(GroupDAO.class.getName()).log(Level.SEVERE, null, ex);
+	    entityManager.getTransaction().commit();
+	    entityManager.close();
+
+	} catch (Exception ex) {
+	    ConsoleLogger.log("Failed trying to get all groups", "DAO", ConsoleLogger.ERROR);
+	    entityManager.close();
+	    return null;
 	}
 	
-	return null;
+	return results;
     }
-    
-    public boolean insertGroup(Group group) {
+
+    @Override
+    public boolean save(Group group) {
+	
+	EntityManager entityManager = JPAUtil.getEntityManagerFactory().createEntityManager();
 	
 	try {
 	    
-	    PreparedStatement ps = con.prepareStatement(INSERT_GROUP);
-	    ps.setString(1, group.getName());
+	    entityManager.getTransaction().begin();
+
+	    entityManager.persist(group);
+
+	    entityManager.getTransaction().commit();
+	    entityManager.close();
 	    
-	    System.out.println("[DB]: Executing sql statement '" + ps.toString() + "'");
-	    ps.executeUpdate();
-	    
-	} catch (SQLException ex) {
-	    System.out.println("[DB]: Could not insert " + group.toString());
-	    Logger.getLogger(GroupDAO.class.getName()).log(Level.SEVERE, null, ex);
+	} catch (Exception ex) {
+	    ConsoleLogger.log("Failed to insert group: " + group.toString(), "DAO", ConsoleLogger.ERROR);
+	    entityManager.close();
 	    return false;
 	}
+	
 	return true;
     }
+
+    @Override
+    public boolean update(Group group) {
+	EntityManager entityManager = JPAUtil.getEntityManagerFactory().createEntityManager();
+	
+	try {
+	    
+	    entityManager.getTransaction().begin(); {
+
+		Group updateGroup = entityManager.find(Group.class, group.getId());
+		updateGroup.setName(group.getName());
+	    }
+	    
+	    entityManager.getTransaction().commit();
+	    entityManager.close();
+
+	} catch (Exception ex) {
+	    ConsoleLogger.log("Failed to update group: " + group.toString(), "DAO", ConsoleLogger.ERROR);
+	    entityManager.close();
+	    return false;
+	}
+	
+	return true;
+    }
+
+    @Override
+    public boolean delete(int id) {
+	EntityManager entityManager = JPAUtil.getEntityManagerFactory().createEntityManager();
+	
+	try {
+	    
+	    entityManager.getTransaction().begin(); {
+
+		Group updateGroup = entityManager.find(Group.class, id);
+		entityManager.remove(updateGroup);
+	    }
+	    
+	    entityManager.getTransaction().commit();
+	    entityManager.close();
+
+	} catch (Exception ex) {
+	    ConsoleLogger.log("Failed to delete group of id: " + id, "DAO", ConsoleLogger.ERROR);
+	    entityManager.close();
+	    return false;
+	}
+	
+	return true;
+    }
+
 }
--- ChaTapp-Server/src/main/java/nhom12/chatapp/server/dao/UserDAO.java
+++ ChaTapp-Server/src/main/java/nhom12/chatapp/server/dao/UserDAO.java
@@ -1,171 +1,223 @@
 package nhom12.chatapp.server.dao;
 
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
 import nhom12.chatapp.model.User;
+import nhom12.chatapp.util.ConsoleLogger;
+import nhom12.hibernate.util.JPAUtil;
 
-public class UserDAO extends DAO {
-
-    private final String CHECK_LOGIN = "SELECT * FROM tbluser WHERE viewname = ? AND password = ?";
-    private final String INSERT_USER = "INSERT INTO tbluser(sdt, password, viewname, fullname,gender, address, dob) VALUES (?,?,?,?,?,?,?);";
-    private final String CHECK_EXIST = "SELECT * FROM tbluser WHERE viewname = ? limit 1";
-    private final String GET_USER_BY_PHONE = "SELECT * FROM tbluser WHERE sdt = ?";
-    private final String GET_ALL_FRIEND = "Select distinct b.*  from tbluser a, tbluser b join tblfriend1 c on (c.user_id = ? and b.id = c.userf_id and c.status = 1) or (c.userf_id = ? and b.id = c.user_id and c.status = 1)";
-    private final String DELETE_FRIEND = "Delete from tblfriend1 where (user_id = ? and userf_id=?) or (user_id = ? and userf_id=?)";
-    private final String GET_ALL_USER = "Select * from tbluser where viewname like ?";
+public class UserDAO implements DAO<User> {
 
     public UserDAO() {
-        super();
+    
     }
     
-    public List<User> getAllUser(String key){
-        PreparedStatement ps;      
-        List<User> listUser = new ArrayList<>();
-        try {
-            ps = con.prepareStatement(GET_ALL_USER);
-            ps.setString(1, '%'+key+'%');
-            ResultSet rs = ps.executeQuery();
-            if (rs.next()) {
-                User user = new User();
-                user.setId(rs.getInt("id"));
-                user.setSdt(rs.getString("sdt"));
-                user.setViewName(rs.getString("viewname"));
-                user.setFullname(rs.getString("fullname"));
-                user.setGender(rs.getString("gender"));
-                user.setAddress(rs.getString("address"));
-                user.setDob(rs.getDate("dob"));
-                listUser.add(user);
-            }
-        } catch (SQLException ex) {
-            Logger.getLogger(UserDAO.class.getName()).log(Level.SEVERE, null, ex);
-        }
-            return listUser;
-    }
-
-    public User checkLogin(String viewname, String password) {
-        
-	User user = null;
-        try {
-	    
-            PreparedStatement ps = con.prepareStatement(CHECK_LOGIN);
-            ps.setString(1, viewname);
-            ps.setString(2, password);
-	    
-            ResultSet rs = ps.executeQuery();
-	    user = new User();
-            if (rs.next()) {
-                user.setId(rs.getInt("id"));
-                user.setViewName(viewname);
-                user.setSdt(rs.getString("sdt"));
-                user.setPassword(password);
-                user.setFullname(rs.getString("fullname"));
-                user.setGender(rs.getString("gender"));
-                user.setAddress(rs.getString("address"));
-                user.setDob(rs.getDate("dob"));
-                user.setStatus("online");
-                PreparedStatement ps1 = con.prepareStatement(GET_ALL_FRIEND);
-                ps1.setInt(1, user.getId());
-                ps1.setInt(2, user.getId());
-                ResultSet rs1 = ps1.executeQuery();
-                List<User> friends = new ArrayList<>();
-                while(rs1.next()){
-                    User friend = new User();
-                    friend.setId(rs1.getInt("id"));
-                    friend.setViewName(rs1.getString("viewname"));
-                    friend.setSdt(rs1.getString("sdt"));
-                    friend.setFullname(rs1.getString("fullname"));
-                    friend.setGender(rs1.getString("gender"));
-                    friend.setAddress(rs1.getString("address"));
-                    friend.setDob(rs1.getDate("dob"));
-                    friends.add(friend);
-                }
-                user.setFriends(friends);
-            }else
-		return null;
-	    
-        } catch (SQLException ex) {
-	    Logger.getLogger(UserDAO.class.getName()).log(Level.SEVERE, null, ex);
-	}
-        return user;
-    }
-
     public boolean checkExist(User user) {
-        PreparedStatement ps;
-        try {
-            ps = con.prepareStatement(CHECK_EXIST);
-            ps.setString(1, user.getSdt());
+	
+	EntityManager entityManager = JPAUtil.getEntityManagerFactory().createEntityManager();
+	
+	try {
 	    
-	    System.out.println("[DB]: Executing sql statement '" + ps.toString() + "'");
-            ResultSet rs = ps.executeQuery();
-            if (rs.next()) {
-                return true;
-            }
-        } catch (SQLException ex) {
-            Logger.getLogger(UserDAO.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        return false;
-    }
+	    entityManager.getTransaction().begin();
+	    
+	    CriteriaBuilder cb = entityManager.getCriteriaBuilder();
+	    CriteriaQuery<User> cr = cb.createQuery(User.class);
+	    Root<User> root = cr.from(User.class);
 
-    public boolean insertUser(User user) {
+	    cr.select(root).where(
+		cb.and(
+		    cb.equal(root.get("id"), user.getId())
+		)
+	    );
 
-        try {
-            PreparedStatement ps = con.prepareStatement(INSERT_USER);
-            ps.setString(1, user.getSdt());
-            ps.setString(2, user.getPassword());
-            ps.setString(3, user.getViewName());
-            ps.setString(4, user.getFullname());
-            ps.setString(5, user.getGender());
-            ps.setString(6, user.getAddress());
-            ps.setDate(7, new java.sql.Date(user.getDob().getTime()));
-            
-            ps.executeUpdate();
+	    Query query = entityManager.createQuery(cr);
+	    boolean result = query.getResultList().size() == 1;
+	    
+	    entityManager.getTransaction().commit();
+	    entityManager.close();
+	    return result;
 
-        } catch (SQLException e) {
-            return false;
-        }
-        return true;
-    }
-    
-    public User getUserByPhone(User user){
-        PreparedStatement p;
-        try {
-            p = con.prepareStatement(GET_USER_BY_PHONE);
-            p.setString(1, user.getSdt());
-            ResultSet rs = p.executeQuery();
-            while (rs.next()) {
-                user.setId(rs.getInt("id"));
-                user.setViewName(rs.getString("viewname"));
-                user.setFullname(rs.getString("fullname"));
-                user.setGender(rs.getString("gender"));
-                user.setAddress(rs.getString("address"));
-                user.setDob(rs.getDate("dob"));
-                user.setStatus("1");
-                return user;
-            }
-        } catch (SQLException ex) {
-            Logger.getLogger(UserDAO.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        return null;
+	} catch (Exception ex) {
+	    
+	    ConsoleLogger.log(
+		"Failed trying to check if user exist: " + user.toString(), 
+		"DAO", 
+		ConsoleLogger.ERROR
+	    );
+	    entityManager.close();
+	}
+	
+	return false;
     }
     
-    public boolean deleteFriend(User user, int id){
-        PreparedStatement ps;
-        try {
-            ps = con.prepareStatement(DELETE_FRIEND);
-            ps.setInt(1, user.getId());
-            ps.setInt(2, id);
-            ps.setInt(3, id);
-            ps.setInt(4, user.getId());
-            ps.executeUpdate();
-            return true;
-        } catch (SQLException ex) {
-            Logger.getLogger(UserDAO.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        return false;
+    public User findByLoginInfo(String username, String password) {
+	
+	User result;
+	EntityManager entityManager = JPAUtil.getEntityManagerFactory().createEntityManager();
+	
+	try {
+	    
+	    entityManager.getTransaction().begin(); {
+	    
+		CriteriaBuilder cb = entityManager.getCriteriaBuilder();
+		CriteriaQuery<User> cr = cb.createQuery(User.class);
+		Root<User> root = cr.from(User.class);
+		
+		cr.select(root).where(
+		    cb.and(
+			cb.equal(root.get("viewname"), username), 
+			cb.equal(root.get("password"), password)
+		    )
+		);
+
+		Query query = entityManager.createQuery(cr);
+		result = (User) query.getSingleResult();
+	    }
+	    
+	    entityManager.getTransaction().commit();
+	    entityManager.close();
+
+	} catch (Exception ex) {
+	    
+	    ConsoleLogger.log(
+		"Failed trying to get user with login info: " + username + ":" + password, 
+		"DAO", 
+		ConsoleLogger.ERROR
+	    );
+	    entityManager.close();
+	    return null;
+	}
+	
+	return result;
+    }
+    
+    @Override
+    public User findById(int id) {
+	
+	User result;
+	EntityManager entityManager = JPAUtil.getEntityManagerFactory().createEntityManager();
+	
+	try {
+	    
+	    entityManager.getTransaction().begin();
+	    
+	    result = entityManager.getReference(User.class, id);
+	    
+	    entityManager.getTransaction().commit();
+	    entityManager.close();
+
+	} catch (Exception ex) {
+	    ConsoleLogger.log("Failed trying to get user of id: " + id, "DAO", ConsoleLogger.ERROR);
+	    entityManager.close();
+	    return null;
+	}
+	
+	return result;
+	
+    }
+    
+    @Override
+    public List<User> findAll() {
+	
+	List<User> results;
+	EntityManager entityManager = JPAUtil.getEntityManagerFactory().createEntityManager();
+	
+	try {
+	    
+	    entityManager.getTransaction().begin(); {
+
+		CriteriaBuilder cb = entityManager.getCriteriaBuilder();
+		CriteriaQuery<User> cr = cb.createQuery(User.class);
+		cr.select(cr.from(User.class));
+
+		Query query = entityManager.createQuery(cr);
+		results = query.getResultList();
+	    }
+	    
+	    entityManager.getTransaction().commit();
+	    entityManager.close();
+
+	} catch (Exception ex) {
+	    ConsoleLogger.log("Failed trying to get all users", "DAO", ConsoleLogger.ERROR);
+	    entityManager.close();
+	    return null;
+	}
+	
+	return results;
+    }
+
+    @Override
+    public boolean save(User user) {
+	
+	EntityManager entityManager = JPAUtil.getEntityManagerFactory().createEntityManager();
+	
+	try {
+	    
+	    entityManager.getTransaction().begin();
+
+	    entityManager.persist(user);
+
+	    entityManager.getTransaction().commit();
+	    entityManager.close();
+	    
+	} catch (Exception ex) {
+	    ConsoleLogger.log("Failed to insert user: " + user.toString(), "DAO", ConsoleLogger.ERROR);
+	    entityManager.close();
+	    return false;
+	}
+	
+	return true;
+    }
+
+    @Override
+    public boolean update(User user) {
+	EntityManager entityManager = JPAUtil.getEntityManagerFactory().createEntityManager();
+	
+	try {
+	    
+	    entityManager.getTransaction().begin(); {
+
+		User updateGroup = entityManager.find(User.class, user.getId());
+//		updateGroup.setName(group.getName());
+	    }
+	    
+	    entityManager.getTransaction().commit();
+	    entityManager.close();
+
+	} catch (Exception ex) {
+	    ConsoleLogger.log("Failed to update user: " + user.toString(), "DAO", ConsoleLogger.ERROR);
+	    entityManager.close();
+	    return false;
+	}
+	
+	return true;
+    }
+
+    @Override
+    public boolean delete(int id) {
+	EntityManager entityManager = JPAUtil.getEntityManagerFactory().createEntityManager();
+	
+	try {
+	    
+	    entityManager.getTransaction().begin(); {
+
+		User updateGroup = entityManager.find(User.class, id);
+		entityManager.remove(updateGroup);
+	    }
+	    
+	    entityManager.getTransaction().commit();
+	    entityManager.close();
+
+	} catch (Exception ex) {
+	    ConsoleLogger.log("Failed to delete user of id: " + id, "DAO", ConsoleLogger.ERROR);
+	    entityManager.close();
+	    return false;
+	}
+	
+	return true;
     }
 }
